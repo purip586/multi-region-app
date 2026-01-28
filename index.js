d4f15ae85b79353bf6f72f9988b9bc58abe9d924
pradip puri <purip586@gmail.com>
2026-01-28 20:19:34 +0900

    Add response timing

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 const REGION = process.env.AWS_REGION || 'local';
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/hello', (req, res) => {
   res.send(`Hello from ${REGION}!`);
 });
 
